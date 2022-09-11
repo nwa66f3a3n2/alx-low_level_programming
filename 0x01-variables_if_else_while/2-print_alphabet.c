6bf83b8aefdde58cc1285d5835cd10e1047d9815
nwa66f3a3n2 <fannwachukwu66@gmail.com>
2022-09-11 16:00:02 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,24 @@
 #include<stdio.h>
 
 /**
- * main-lowercase alphabets
+ * main - lowercase alphabets
  *
  * Return:Always(Success)
  */
+
 int main(void)
 
 	char c;
 
-	for(c ='a';c <= 'z';c++)
+
+	for(c ='a'; c <= 'z'; c++)
+
 
 	       putchar(c);
 
 
        putchar('\n');
+
 return (0);
+
 }
