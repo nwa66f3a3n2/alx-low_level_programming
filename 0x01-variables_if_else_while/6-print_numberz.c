6020675d0d4c105ee1b3ae174b30e244c84ad456
nwa66f3a3n2 <fannwachukwu66@gmail.com>
2022-09-12 14:57:32 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 /**
- *  * main - main function
- *   * Description:printinggg single digits numbers of base 10 starting from 01
- *    * Return: 0
- *     */
+ * main - main function
+ * Description:printinggg single digits numbers of base 10 starting from 01
+ * Return: 0
+ */
 
 int main(void)
 {
-		int x;
+int x;
 
-			for (x = 48; x <= 57; x++)
+	for (x = 48; x <= 57; x++)
 					{
 								putchar(x);
 									}
