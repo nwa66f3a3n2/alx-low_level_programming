d5ed6c203494af45795258daeacfaf2a20f7989c
nwa66f3a3n2 <fannwachukwu66@gmail.com>
2022-09-12 16:37:12 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -8,10 +8,10 @@ int main(void)
 {
 		char c = 'z';
 
-		while (c >= 'a')
+	        while (c >= 'a')
 		{
-		         putchar(c);
-			 c--;
+		        putchar(c);
+			c--;
 		}
 		putchar('\n');
 		return (0);
