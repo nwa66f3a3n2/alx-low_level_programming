c6f0f0e76dab87de3a1c7e637e95d47f17251c8f
nwa66f3a3n2 <fannwachukwu66@gmail.com>
2022-09-12 16:47:58 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -11,10 +11,10 @@ int main(void)
 int x;
 
 	for (x = 48; x <= 57; x++)
-					{
-								putchar(x);
-									}
-				putchar('\n');
+	{
+                 putchar(x);
+	}
+	putchar('\n');
 
-					return (0);
+	return (0);
 }
