354f221429e361404272f4300dba6a42c1a5c906
nwa66f3a3n2 <fannwachukwu66@gmail.com>
2022-09-12 16:09:53 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,14 +8,15 @@
 int main(void)
 {
 	int num;
-		
+	
 	for (num = 0; num <= 9; num++)
 	{
 		putchar((num % 10) + '0');
 		if (num == 9)
 			continue;
 
-		 putchar(',');												      putchar(' ');
+		 putchar(',');
+		 putchar(' ');
 	}
 
 	putchar('\n');
